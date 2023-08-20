c3e3d813143d781ab73508faed128997f53add89
Adam Wilczek <adamwilczek@pragmative.io>
2023-08-20 12:26:55 +0200

    server running:

--- 02_compute.tf
+++ 02_compute.tf
@@ -17,8 +17,12 @@ data "aws_ami" "os" {
 resource "tls_private_key" "private_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
-}
 
+}
+resource "local_file" "cloud_pem" {
+  filename = "${path.module}/cloudtls.pem"
+  content  = tls_private_key.private_key.private_key_pem
+}
 resource "aws_key_pair" "generated_key" {
   key_name   = "${var.main.name}-key"
   public_key = tls_private_key.private_key.public_key_openssh
